eedd37e5bf49d39f4769f259522db2b05c11f847
davidhanenko <d.hanenko24@gmail.com>
2021-04-22 18:37:16 -0400

    add border

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,10 @@
-export function addBorder(picture: string[]): string[] {
+export function addBorder(picture: string[]): any {
+  const borderBase = '*'.repeat(picture[0].length);
 
+  picture.unshift(borderBase);
+  picture.push(borderBase);
+
+  return picture.map(el => `*${el}*`);
 }
 
-// console.log(addBorder(["abc", "ded"]));
+// console.log(addBorder(['abc', 'ded']));
