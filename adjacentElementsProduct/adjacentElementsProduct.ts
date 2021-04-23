6c1ac2b09ff47191207c98b4650023a40cf51fd2
davidhanenko <d.hanenko24@gmail.com>
2021-04-22 20:36:21 -0400

    lg product

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,12 @@
 export function adjacentElementsProduct(inputArray: number[]): number {
+  let maxProduct = inputArray[0] * inputArray[1];
 
+  for (let i = 1; i < inputArray.length; i++) {
+    let product = inputArray[i] * inputArray[i - 1];
+
+    maxProduct = maxProduct < product ? product : maxProduct;
+  }
+
+  return maxProduct;
 }
-
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
