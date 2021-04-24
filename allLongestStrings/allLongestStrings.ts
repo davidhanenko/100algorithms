4905e50de34035039bb4738ba0a8064f41f9385d
davidhanenko <d.hanenko24@gmail.com>
2021-04-23 21:53:22 -0400

    all longest strings

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,6 @@
 export function allLongestStrings(inputArray: string[]): string[] {
-
+  let max = Math.max(...inputArray.map(el => el.length));
+return inputArray.filter(el => el.length === max )
 }
 
-console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+// console.log(allLongestStrings(['aba', 'aa', 'ad', 'vcd', 'aba']));
