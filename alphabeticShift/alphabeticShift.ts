174d91f4a356b7b042bad7b9f158d7fefeb5c3a4
davidhanenko <d.hanenko24@gmail.com>
2021-04-27 22:14:44 -0400

    alphabet

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,12 +1,13 @@
 export function alphabeticShift(inputString: string): string {
   let alphabet = [...Array(26).keys()].map(i => String.fromCharCode(i + 97));
-  return inputString
-    .split('')
-    .forEach((el, i) => {
-      let index = inputString.indexOf(el);
-      el = inputString(index + 1);
-    })
-    .join('');
+
+  let newStr = inputString.split('');
+  for (let i = 0; i < newStr.length; i++) {
+    newStr[i] === 'z'
+      ? (newStr[i] = alphabet[0])
+      : (newStr[i] = alphabet[alphabet.indexOf(newStr[i]) + 1]);
+  }
+  return newStr.join('');
 }
 
 console.log(alphabeticShift('crazy'));
