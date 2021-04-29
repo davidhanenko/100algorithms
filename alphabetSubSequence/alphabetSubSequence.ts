ac199e60c344a34c8acc409f082107faab710bbb
davidhanenko <d.hanenko24@gmail.com>
2021-04-28 22:10:42 -0400

    altSum

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,16 +1,23 @@
 export function alphabetSubsequence(s: string): boolean {
-  let alphabet = [...Array(26).keys()].map(i => String.fromCharCode(i + 97));
+  // let alphabet = [...Array(26).keys()].map(i => String.fromCharCode(i + 97));
 
-  for (let i = 0; i < s.length-1; i++) {
-    if (alphabet.indexOf(s[i]) >= alphabet.indexOf(s[i + 1])) {
+  // for (let i = 0; i < s.length-1; i++) {
+  //   if (alphabet.indexOf(s[i]) >= alphabet.indexOf(s[i + 1])) {
+  //     return false;
+  //   }
+  // }
+
+  let arr = s.split('')
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i].charCodeAt(0) >= arr[i+1].charCodeAt(0)) {
       return false;
     }
   }
   return true;
 }
 
-console.log(alphabetSubsequence('zab'));
-console.log(alphabetSubsequence('effg'));
-console.log(alphabetSubsequence('cdce'));
-console.log(alphabetSubsequence('ace'));
-console.log(alphabetSubsequence('bxz'));
+// console.log(alphabetSubsequence('zab'));
+// console.log(alphabetSubsequence('effg'));
+// console.log(alphabetSubsequence('cdce'));
+// console.log(alphabetSubsequence('ace'));
+// console.log(alphabetSubsequence('bxz'));
--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,11 @@
 export function alternatingSums(a: number[]): number[] {
+  let team1 = 0;
+  let team2 = 0;
 
+  a.forEach((el, i) => {
+    i % 2 === 0 ? (team1 += el) : (team2 += el);
+  });
+  return [team1, team2];
 }
 
-console.log(alternatingSums([50, 60, 60, 45, 70]))
+// console.log(alternatingSums([50, 60, 60, 45, 70]));
