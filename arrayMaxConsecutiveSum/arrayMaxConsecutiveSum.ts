4ad490536c579f4b23f62e17dad4d39791c80f47
davidhanenko <d.hanenko24@gmail.com>
2021-04-30 23:54:09 -0400

    arrayMaxConsecutiveSum

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,13 +1,22 @@
-export function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
- let sum = 0;
-  for (let i=0; i<k; i++) {
-    sum +=inputArray[i]
+export function arrayMaxConsecutiveSum(
+  inputArray: number[],
+  k: number
+): number {
+  let currentSum = 0;
+  let maxSum;
+  for (let i = 0; i < k; i++) {
+    currentSum += inputArray[i];
   }
-    let max = sum
- for(let i=k; i<inputArray.length; i++) {
-                                                                                                                                                                                                                                                                                                                                                                           
- }
-    
+  maxSum = currentSum;
+  let j = 0;
+  for (let i = k; i < inputArray.length; i++) {
+    currentSum = currentSum - inputArray[j] + inputArray[i];
+    j++;
+    if (maxSum <= currentSum) {
+      maxSum = currentSum;
+    }
+  }
+  return maxSum;
 }
 
 console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
