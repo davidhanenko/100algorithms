2bd26e1aeaf563cd50eab4d7ba2996f6f07f8f60
davidhanenko <d.hanenko24@gmail.com>
2021-05-04 18:10:04 -0400

    maxdiff refactor

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,9 +1,9 @@
 export function arrayMaximalAdjacentDifference(inputArray: number[]): number {
-  let maxDiff = [];
+  let maxDiff = 0;
   for (let i = 1; i < inputArray.length; i++) {
-    maxDiff.push(inputArray[i - 1] - inputArray[i]);
+    maxDiff < (inputArray[i - 1] - inputArray[i]) ? maxDiff = (inputArray[i - 1] - inputArray[i]) : maxDiff
   }
-  return Math.max(...maxDiff);
+  return maxDiff
 }
 
 console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
