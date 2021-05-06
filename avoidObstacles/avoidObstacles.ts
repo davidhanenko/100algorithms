ef380a6068fc3e5e88bcc78219bd81e34268f239
davidhanenko <d.hanenko24@gmail.com>
2021-05-06 19:31:19 -0400

    avoidObstacles

--- avoidObstacles/avoidObstacles.ts
+++ avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,12 @@
 export function avoidObstacles(inputArray: number[]): number {
+  inputArray.sort((a, b) => a - b);
 
+  for (let i = 0; i < inputArray.length; i++) {
+    let temp = inputArray[i];
+
+    if (inputArray.every(el => el !== temp + 1)) {
+      return temp + 1;
+    }
+  }
 }
-
-// console.log(avoidObstacles([5, 3, 6, 7, 9]));
+console.log(avoidObstacles([5, 3, 6, 7, 9]));
