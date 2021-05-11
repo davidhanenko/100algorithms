ce73096eb17cb4c4868d26f1001a3e46abf112fd
davidhanenko <d.hanenko24@gmail.com>
2021-05-11 19:56:15 -0400

    chessBoardCellColor

--- chessBoardCellColor/chessBoardCellColor.ts
+++ chessBoardCellColor/chessBoardCellColor.ts
@@ -1,4 +1,24 @@
+import { parseJsonConfigFileContent } from 'typescript';
+
 export function chessBoardCellColor(cell1: string, cell2: string): boolean {
+  const board = {
+    A: 1,
+    B: 2,
+    C: 3,
+    D: 4,
+    E: 5,
+    F: 6,
+    G: 7,
+    H: 8,
+  };
+
+  let cell1Color =
+    (board[cell1[0]] + (parseInt(cell1[1]))) % 2 === 0 ? 'brown' : 'white';
+
+  let cell2Color =
+    (board[cell2[0]] + (parseInt(cell2[1]))) % 2 === 0 ? 'brown' : 'white';
+
+return cell1Color === cell2Color;
 
 }
 
