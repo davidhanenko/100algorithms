20cf9c410857e2a06c3d4f58ce62831911e453e8
davidhanenko <d.hanenko24@gmail.com>
2021-05-13 19:00:31 -0400

    chunkeyMonkey

--- chunkyMonkey/chunkyMonkey.ts
+++ chunkyMonkey/chunkyMonkey.ts
@@ -1,6 +1,6 @@
 export function chunkyMonkey(arr: any[], size: number): any[][] {
-
+ return [arr.slice(0, size), arr.slice(size)]
 }
 
-// console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
-// console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
+console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
+console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
