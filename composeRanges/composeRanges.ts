144d9c3b810c3b4a94f34d9b8cc7ffbb47e987c1
davidhanenko <d.hanenko24@gmail.com>
2021-06-06 21:44:28 -0400

    range

--- composeRanges/composeRanges.ts
+++ composeRanges/composeRanges.ts
@@ -1,5 +1,18 @@
 export function composeRanges(nums: number[]): string[] {
+  const res = [];
+  let start = nums[0];
 
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] - nums[i-1] !== 1) {
+      res.push(`${start}->${nums[i-1]}`);
+      start = nums[i];
+    } 
+    if(start === nums[i] && !nums[i+1]) {
+      res.push(`${nums[i]}`)
+    }
+  }
+  console.log(res)
+  return res
 }
 
-// console.log(composeRanges([-1, 0, 1, 2, 6, 7, 9]));
+console.log(composeRanges([-1, 0, 1, 2, 6, 7, 9]));
