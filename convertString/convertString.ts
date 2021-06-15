bb5383b4b3e8dece916db9798e927b3321a06bd9
davidhanenko <d.hanenko24@gmail.com>
2021-06-14 22:47:54 -0400

    convertString

--- convertString/convertString.ts
+++ convertString/convertString.ts
@@ -1,5 +1,10 @@
 export function convertString(s: string, t: string): boolean {
-
+  for(let i=0; i<t.length; i++) {
+    if(s.includes(t[i])) {
+     s = s.slice(s.indexOf(t[i]))
+    } else return false;
+  }
+  return true
 }
 
 // console.log(convertString('ceoydefthf5iyg5h5yts', 'codefights'));
