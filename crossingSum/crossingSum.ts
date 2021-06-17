27dfe36a2c8e1f728e7213cfb5f2ed22ac9c0f98
davidhanenko <d.hanenko24@gmail.com>
2021-06-16 22:16:27 -0400

    crossingSum

--- crossingSum/crossingSum.ts
+++ crossingSum/crossingSum.ts
@@ -1,7 +1,17 @@
 export function crossingSum(matrix: number[][], a: number, b: number): number {
-
+  let row = matrix[a].reduce((a, b) => a + b, 0);
+  let col = matrix.map(el => (el = el[b])).reduce((a, b) => a + b);
+  return row + col - matrix[a][b];
 }
 
-// console.log(crossingSum([[1, 1, 1, 1], 
-//     [2, 2, 2, 2], 
-//     [3, 3, 3, 3]], 1, 3));
+// console.log(
+//   crossingSum(
+//     [
+//       [1, 1, 1, 1],
+//       [2, 2, 2, 2],
+//       [3, 3, 3, 3],
+//     ],
+//     1,
+//     3
+//   )
+// );
