87c9893b20a86cce635bdace61b072043e8b772a
davidhanenko <d.hanenko24@gmail.com>
2021-06-16 22:54:59 -0400

    differentSymbolsNaive

--- differentSymbolsNaive/differentSymbolsNaive.ts
+++ differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,5 +1,10 @@
 export function differentSymbolsNaive(s: string): number {
+  let obj = {};
 
+  for (let char of s.split('')) {
+    obj.hasOwnProperty(char) ? (obj[char] += 1) : (obj[char] = 1);
+  }
+  return Object.keys(obj).length;
 }
 
-// console.log(differentSymbolsNaive('cabca'));
+console.log(differentSymbolsNaive('cabca'));
