e82a3df57b5d1ab53140d398d0c1391b6f9a474a
davidhanenko <d.hanenko24@gmail.com>
2021-06-26 11:43:20 -0400

    digitalDegree

--- digitDegree/digitDegree.ts
+++ digitDegree/digitDegree.ts
@@ -1,5 +1,20 @@
 export function digitDegree(n: number): number {
+  let count = 0;
 
+  if (String(n).split('').length === 1) return 0;
+  
+  count++;
+
+  n = String(n)
+    .split('')
+    .map(el => Number(el))
+    .reduce((a, b) => a + b);
+
+  if (String(n).split('').length !== 1) {
+    digitDegree(n);
+  }
+
+  return count;
 }
 
 // console.log(digitDegree(5));
