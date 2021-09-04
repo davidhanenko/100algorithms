82c347957a4551da198ca9c26ba8c27ebb986a89
davidhanenko <d.hanenko24@gmail.com>
2021-09-04 14:21:30 -0400

    arrayConversion

--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -1,22 +1,32 @@
 export function arrayConversion(inputArray: number[]): number {
-  let l = inputArray.length;
-  var newArr = [];
-  let i = 1;
+  let iteration = 1;
+  let iterations = inputArray.length / 2;
 
-  if (i % 2 !== 0) {
-  
-  }
-
-  if (i % 2 === 0) {
-    for (let j = 1; j < l; j+=2) {
-      newArr.push(newArr[j] * newArr[j - 1]);
+  for (let i = 0; i < iterations - 1; i++) {
+    if (iteration % 2 !== 0) {
+      inputArray = sum(inputArray, iteration);
+      iteration++;
+    } else {
+      inputArray = prod(inputArray, iteration);
+      iteration++;
     }
-    inputArray = newArr.slice(l / 2);
-    l = inputArray.length;
-    i++;
   }
-  console.log(inputArray);
+  return inputArray[0];
 }
 
+const sum = (arr, iteration) => {
+  let tempArr = [];
+  for (let i = 1; i < arr.length; i += 2) {
+    tempArr.push(arr[i] + arr[i - 1]);
+  }
+  return tempArr;
+};
 
+const prod = (arr, iteration) => {
+  let tempArr = [];
+  for (let i = 1; i < arr.length; i += 2) {
+    tempArr.push(arr[i] * arr[i - 1]);
+  }
+  return tempArr;
+};
 // console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
