178a1a4e031a01ecf921f6ae054197529882fa55
davidhanenko <d.hanenko24@gmail.com>
2021-09-03 21:45:28 -0400

    arrayChange

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,24 @@
-export function arrayChange(inputArray: number[]): number {
+export function arrayChange( inputArray: number[] ): number {
 
+  let moves = 0;
+  for (let i = 1; i < inputArray.length; i++) {
+    while (inputArray[i] - inputArray[i - 1] !== 1) {
+      inputArray[i]++;
+      moves++
+    }
+  }
+  return moves;
+
+  //  let value_increased = 0;
+  //  for (let i = 0; i < inputArray.length - 1; i++) {
+  //    if (inputArray[i] >= inputArray[i + 1]) {
+  //      let temp = inputArray[i] - inputArray[i + 1] + 1;
+  //      value_increased += temp;
+  //      inputArray[i + 1] += temp;
+  //    }
+  //  }
+  //  return value_increased;
 }
 
+// console.log(arrayChange([5, 3, 1]));
 // console.log(arrayChange([1, 1, 1]));
