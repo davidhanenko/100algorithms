f7eef0ff2e885df5ad96c96011ccee5456fd3806
davidhanenko <d.hanenko24@gmail.com>
2021-09-18 14:54:50 -0400

    findClosestPair - refactor

--- findClosestPair/findClosestPair.ts
+++ findClosestPair/findClosestPair.ts
@@ -2,20 +2,21 @@ export function findClosestPair(numbers: number[], sum: number): number {
 
   const map = new Map();
 
-  let minDiff = -1;
+  let minDiff = numbers.length - 1;
+  let isPair = 0;
 
   for ( let i = 0; i < numbers.length; i++ ) {
     
     let diff = sum - numbers[i];
 
     if ( map.has( diff ) ) {
-      minDiff = numbers.length - 1;
-      minDiff = Math.min( minDiff, i - map.get(diff))
+      minDiff = Math.min( minDiff, i - map.get( diff ) );
+      isPair++
     }
     map.set( numbers[i], i );
   }
-  return minDiff;
-};
+  return isPair > 0 ? minDiff : -1;
+}
 
 // console.log(findClosestPair([1, 0, 2, 4, 3, 0], 5));
 // console.log(findClosestPair([2, 3, 7], 8));
