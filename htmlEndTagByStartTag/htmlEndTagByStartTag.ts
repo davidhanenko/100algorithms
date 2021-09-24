658a929d66db8bba1c00e9d5ae95186c77a6d9d5
davidhanenko <d.hanenko24@gmail.com>
2021-09-23 23:53:15 -0400

    htmlEndTagByStartTag

--- htmlEndTagByStartTag/htmlEndTagByStartTag.ts
+++ htmlEndTagByStartTag/htmlEndTagByStartTag.ts
@@ -1,5 +1,10 @@
 export function htmlEndTagByStartTag(startTag: string): string {
+  let arr = startTag.split('');
+  arr.splice(1, 0, '/');
 
+  arr = arr.join('').split(' ')[0];
+
+  return arr[arr.length - 1] === '>' ? arr : (arr += '>');
 }
 
 // console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
