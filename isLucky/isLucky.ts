9aa078676690869be47dac8032fa47a62e017891
davidhanenko <d.hanenko24@gmail.com>
2022-01-08 15:40:46 -0500

    isLucky

--- isLucky/isLucky.ts
+++ isLucky/isLucky.ts
@@ -1,6 +1,18 @@
 export function isLucky(n: number): boolean {
 
+  const nArr = n.toString().split( '' );
+  const half = nArr.length / 2;
+
+  const left = nArr.slice( 0, half );
+  const right = nArr.slice(half);
+  
+  const sum = ( arr ) => {
+    return arr.reduce( ( a, b ) => +a + +b );
+ }
+
+  return sum( left ) === sum( right ) ? true : false;
+  
 }
 
-// console.log(isLucky(1230));
-// console.log(isLucky(239017));
+console.log(isLucky(1230));
+console.log(isLucky(239017));
