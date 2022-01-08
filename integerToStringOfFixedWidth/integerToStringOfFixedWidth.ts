0dfee5ad1dc9ebd17e9d8c3203e3765a615de170
davidhanenko <d.hanenko24@gmail.com>
2022-01-08 15:31:29 -0500

    integerToStringLength

--- integerToStringOfFixedWidth/integerToStringOfFixedWidth.ts
+++ integerToStringOfFixedWidth/integerToStringOfFixedWidth.ts
@@ -1,7 +1,17 @@
-export function integerToStringOfFixedWidth(number: number, width: number): string {
+export function integerToStringOfFixedWidth(
+  number: number,
+  width: number
+): string {
 
+  let strNum = number.toString();
+
+  if ( strNum.length === width ) {
+    return strNum;
+  } else {
+    return strNum.length < width ? strNum.padStart( width, '0' ) : strNum.slice( width ).padStart( width, '' );
+  }
 }
 
-// console.log(integerToStringOfFixedWidth(1234, 2));
-// console.log(integerToStringOfFixedWidth(1234, 4));
-// console.log(integerToStringOfFixedWidth(1234, 5));
+console.log(integerToStringOfFixedWidth(1234, 2));
+console.log(integerToStringOfFixedWidth(1234, 4));
+console.log(integerToStringOfFixedWidth(1234, 5));
