139b8d8c8946ba60887ad9dc79e75254ee45d70c
davidhanenko <d.hanenko24@gmail.com>
2022-02-07 21:17:21 -0500

    launch seq

--- launchSequenceChecker/launchSequenceChecker.ts
+++ launchSequenceChecker/launchSequenceChecker.ts
@@ -1,6 +1,41 @@
-export function launchSequenceChecker(systemNames: string[], stepNumbers: number[]): boolean {
+export function launchSequenceChecker(
+  systemNames: string[],
+  stepNumbers: number[]
+): boolean {
+  const obj = {};
 
+  for (let i = 0; i < systemNames.length; i++) {
+    if (obj.hasOwnProperty(systemNames[i])) {
+      if (obj[systemNames[i]] > stepNumbers[i]) {
+        return false;
+      } else {
+        obj[systemNames[i]] = stepNumbers[i];
+      }
+    } else {
+      obj[systemNames[i]] = stepNumbers[i];
+    }
+  }
+  console.log(obj);
+
+  return true;
 }
 
-// console.log(launchSequenceChecker(["stage_1", "stage_2", "dragon", "stage_1", "stage_2", "dragon"], [1, 10, 11, 2, 12, 111]));
-// console.log(launchSequenceChecker(["stage_1", "stage_1", "stage_2", "dragon"], [2, 1, 12, 111]));
+// console.log(
+//   launchSequenceChecker(
+//     [
+//       'stage_1',
+//       'stage_2',
+//       'dragon',
+//       'stage_1',
+//       'stage_2',
+//       'dragon',
+//     ],
+//     [1, 10, 11, 2, 12, 111]
+//   )
+// );
+// console.log(
+//   launchSequenceChecker(
+//     ['stage_1', 'stage_1', 'stage_2', 'dragon'],
+//     [2, 1, 12, 111]
+//   )
+// );
