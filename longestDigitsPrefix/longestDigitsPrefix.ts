32de10c221a6aba776f60c556d05f4a2980c27f3
davidhanenko <d.hanenko24@gmail.com>
2022-02-07 21:54:40 -0500

    longest digits prefix

--- longestDigitsPrefix/longestDigitsPrefix.ts
+++ longestDigitsPrefix/longestDigitsPrefix.ts
@@ -1,5 +1,11 @@
-export function longestDigitsPrefix(inputString: string): string {
+export function longestDigitsPrefix(
+  inputString: string
+): string {  
+  let regex = /[a-zA-z]/g;
 
+  let i = inputString.search( regex );
+  
+  return inputString.slice( 0, i );
 }
 
-// console.log(longestDigitsPrefix('123aa1'));
+console.log(longestDigitsPrefix('123aa1'));
