24946aeafbe80683239f261ecd8a16914785acc0
davidhanenko <d.hanenko24@gmail.com>
2022-03-24 19:50:35 -0400

    pigLatin

--- pigLatin/pigLatin.ts
+++ pigLatin/pigLatin.ts
@@ -1,6 +1,19 @@
 export function pigLatin(str: string): string {
+  const regex = /[aeiou]/;
+  let temp = '';
+  let i = 0;
 
+  if (regex.test(str[0])) {
+    return `${str}way`;
+  } else {
+    while (!regex.test(str[i])) {
+      temp += str[i];
+      i++;
+    }
+    return str.slice(i) + temp + 'ay';
+  }
 }
 
-// console.log(pigLatin("glove"));
-// console.log(pigLatin("eight"));
+// console.log(pigLatin('glove'));
+// console.log(pigLatin('eight'));
+// console.log(pigLatin('koight'));
